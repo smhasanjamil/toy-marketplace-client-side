2402239cb701298e781c1dec848c748ecd7d2e07
smhasanjamil <smhasanjamil23@gmail.com>
2023-05-21 23:01:05 +0600

    AOS animation added

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -1,17 +1,28 @@
+import Aos from "aos";
+import 'aos/dist/aos.css';
 import ShopByCategory from "../ShopByCategory/ShopByCategory";
 import CustomerReview from "./CustomerReview/CustomerReview";
 import Gallery from "./Gallery/Gallery";
 import Hero from "./Hero";
 import OurService from "./OurService";
+import { useEffect } from "react";
+
 
 
 const Home = () => {
 
+    useEffect(() => {
+        Aos.init();
+    }, [])
     return (
         <div className="container mx-auto px-2">
             <Hero />
-            <Gallery />
-            <ShopByCategory />
+            <div data-aos="fade-right" >
+                <Gallery />
+            </div>
+            <div data-aos="fade-left" >
+                <ShopByCategory />
+            </div>
             <OurService />
             <CustomerReview />
         </div>
